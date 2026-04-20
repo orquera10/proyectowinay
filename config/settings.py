fc391809103ca64b57537429cd58f270270b03bf
dario orquera <darorquera@snaf.mdsjujuy.gov.ar>
2026-04-20 12:54:20 -0300

    feat: solucionando imagenes

--- config/settings.py
+++ config/settings.py
@@ -115,12 +115,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = Path(os.getenv('MEDIA_ROOT', BASE_DIR / 'media'))
 SERVE_MEDIA = os.getenv('SERVE_MEDIA', 'True').lower() in {'1', 'true', 'yes', 'on'}
 
